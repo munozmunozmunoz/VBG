3fb666dcf2563938086db793c4fff3b2ce81fd38
munozmunozmunoz <djm516@psu.edu>
2015-07-30 09:48:31 -0400

    Remove unnecessary initial value

--- IndividualGrowth/Scripts/Morph_4SeasonDiff_VBG.R
+++ IndividualGrowth/Scripts/Morph_4SeasonDiff_VBG.R
@@ -14,7 +14,7 @@ data <- list(Lr=dat$SVL.1, Lm = dat$SVL, dSp = dat$dSp, dSm = dat$dSm,
 
 
 # Initial values
-inits <- function(){list(L.inf = rnorm(2,0,0.001), K = rnorm(4,1,0.001))}
+inits <- function(){list(L.inf = rnorm(2,0,0.001))}
 
 # Parameters monitored
 params1 <- c("L.inf", "Ksp", "Ksm","Kf","Kw", "sigma.Lr","tau.Lr")
