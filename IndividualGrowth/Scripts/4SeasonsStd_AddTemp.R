fe3b5d3e5704f118fffa376f9b322194476e69f3
munozmunozmunoz <djm516@psu.edu>
2015-10-26 09:00:35 -0400

    Add Temperature to 4 Std Seasons
    Temperature is now in the data for the four std seasons. Models ran and
    converged, but it is estimating a separate growth parameter for every
    observation. Will fix

--- IndividualGrowth/Scripts/4SeasonsStd_AddTemp.R
+++ IndividualGrowth/Scripts/4SeasonsStd_AddTemp.R
@@ -2,9 +2,8 @@
 ### Add cool/warm season days ###
 #################################
 setwd("E:/Current/New or Updated Files/PennState/Research/CMH/IndividualGrowth")
-temp<-read.csv("Data/4SeasonTemp.csv")
-temp<-temp[,-1]
-dat<-read.csv("Data/VBG_4SeasonDiffTemp.csv")
+temp<-read.csv("Data/Patuxent_Weather_VBGData.csv")
+dat<-read.csv("Data/VBG_4SeasonStd.csv")
 #############Summer days vector approach
 #Day 0 to day 1307 (1308 total days)
 summerdays<-vector("numeric", length=1308)
@@ -12,10 +11,6 @@ winterdays<-vector("numeric", length=1308)
 springdays<-vector("numeric", length=1308)
 falldays<-vector("numeric", length=1308)
 
-dat$dSp=0
-dat$dSm=0
-dat$dF=0
-dat$dW=0
 #1s indicate a fall day based on dates surveyed
 #thus each fall has a different number of days
 falldays[1:50]<-1
@@ -34,45 +29,58 @@ falldays[1057:1147]<-1
 winterdays[1148:1233]<-1
 springdays[1234:1308]<-1
 
-#For each record, create number of summer days and number of winters days
-for(i in 1:dim(dat)[1]){
-  dat$dSp[i]<-sum(springdays[(dat$Days[i]+1):(dat$Days.1[i]+1)])
-  dat$dSm[i]<-sum(summerdays[(dat$Days[i]+1):(dat$Days.1[i]+1)])
-  dat$dF[i]<-sum(falldays[(dat$Days[i]+1):(dat$Days.1[i]+1)])
-  dat$dW[i]<-sum(winterdays[(dat$Days[i]+1):(dat$Days.1[i]+1)])
-}
-#Individuals caught on first day of season don't experience that season's growth
-dat$dSp[which(dat$dSp==1)]<-0
-dat$dF[which(dat$dF==1)]<-0
-
-dat<-droplevels(dat)
+temp$StdSp<-0
+temp$StdSm<-0
+temp$StdF<-0
+temp$StdW<-0
 
 #Standardize each winter and summer(single value for all W/Sm days)
-sm1<-mean(temp[201:325,7], na.rm=T)
-sm2<-mean(temp[561:718,7], na.rm=T)
-sm3<-mean(temp[930:1071,7], na.rm=T)
-StdSm<-scale(c(sm1,sm2,sm3), center=TRUE, scale=TRUE)
+sm1<-mean(temp[214:325,5], na.rm=T)
+sm2<-mean(temp[579:690,5], na.rm=T)
+sm3<-mean(temp[945:1056,5], na.rm=T)
+StdSm<-scale(c(sm1, sm2,sm3), center=TRUE, scale=TRUE)
 
-w1<-mean(temp[51:143,9], na.rm=T)
-w2<-mean(temp[400:502,9], na.rm=T)
-w3<-mean(temp[764:872,9], na.rm=T)
-w4<-mean(temp[1132:1267,9], na.rm=T)
+w1<-mean(temp[51:137,5], na.rm=T)
+w2<-mean(temp[416:502,5], na.rm=T)
+w3<-mean(temp[781:868,5], na.rm=T)
+w4<-mean(temp[1148:1233,5], na.rm=T)
 StdW<-scale(c(w1,w2,w3,w4), center=TRUE, scale=TRUE)
 
-temp$StdSm[201:325]<-StdSm[1]
-temp$StdSm[561:718]<-StdSm[2]
-temp$StdSm[930:1071]<-StdSm[3]
-temp$StdW[51:143]<-StdW[1]
-temp$StdW[400:502]<-StdW[2]
-temp$StdW[764:872]<-StdW[3]
-temp$StdW[1132:1267]<-StdW[4]
-###### Standardize low temperature values for spring/fall
-#Z-standardize both temp columns
-temp$StdSp<-scale(as.numeric(temp$TempSp), center=TRUE, scale=TRUE)
-temp$StdF<-scale(as.numeric(temp$TempF), center=TRUE, scale=TRUE)
+sp1<-mean(temp[138:213,5], na.rm=T)
+sp2<-mean(temp[503:578,5], na.rm=T)
+sp3<-mean(temp[869:944,5], na.rm=T)
+sp4<-mean(temp[1234:1308,5], na.rm=T)
+StdSp<-scale(c(sp1,sp2,sp3,sp4), center=TRUE, scale=TRUE)
+
+f1<-mean(temp[1:50,5], na.rm=T)
+f2<-mean(temp[326:415,5], na.rm=T)
+f3<-mean(temp[691:780,5], na.rm=T)
+f4<-mean(temp[1057:1147,5], na.rm=T)
+StdF<-scale(c(f1,f2,f3,f4), center=TRUE, scale=TRUE)
+
+temp$StdSm[214:325]<-StdSm[1]
+temp$StdSm[579:690]<-StdSm[2]
+temp$StdSm[945:1056]<-StdSm[3]
+temp$StdW[51:137]<-StdW[1]
+temp$StdW[416:502]<-StdW[2]
+temp$StdW[781:868]<-StdW[3]
+temp$StdW[1148:1233]<-StdW[4]
+temp$StdSp[138:213]<-StdSp[1]
+temp$StdSp[503:578]<-StdSp[2]
+temp$StdSp[869:944]<-StdSp[3]
+temp$StdSp[1234:1308]<-StdSp[4]
+temp$StdF[1:50]<-StdF[1]
+temp$StdF[326:415]<-StdF[2]
+temp$StdF[691:780]<-StdF[3]
+temp$StdF[1057:1147]<-StdF[4]
 
 ###### Add mean temperature to time intervals
 str(dat)
+
+dat$TempSp<-0
+dat$TempSm<-0
+dat$TempF<-0
+dat$TempW<-0
 #For each record, take mean of temp of each season between two days
 for(i in 1:dim(dat)[1]){
   dat$TempSp[i]<-mean(temp$StdSp[dat$Days[i]:dat$Days.1[i]], na.rm=T)
@@ -80,13 +88,7 @@ for(i in 1:dim(dat)[1]){
   dat$TempF[i]<-mean(temp$StdF[dat$Days[i]:dat$Days.1[i]], na.rm=T)
   dat$TempW[i]<-mean(temp$StdW[dat$Days[i]:dat$Days.1[i]], na.rm=T)  
 }
-#Change NAs to zeroes so BUGS doesn't explode
-dat$TempSp[which(dat$TempSp=="NaN")]<-0
-dat$TempSm[which(dat$TempSm=="NaN")]<-0
-dat$TempF[which(dat$TempF=="NaN")]<-0
-dat$TempW[which(dat$TempW=="NaN")]<-0
 
 #Write new data files
-dat<-dat[,c(-1,-2,-9,-10)]
-write.csv(dat, file="Data/VBG_4SeasonDiffTemp.csv")
+write.csv(dat, file="Data/VBG_4SeasonStdTemp.csv")
 #write.csv(temp, file="Data/4SeasonTemp.csv")
--- IndividualGrowth/Scripts/Morph_4SeasonStdTemp_VBG.R
+++ IndividualGrowth/Scripts/Morph_4SeasonStdTemp_VBG.R
@@ -2,22 +2,24 @@
 ### Von Bertalanffy Growth ###
 ##############################
 
-dat<-read.csv("Data/VBG_4SeasonStd.csv")
+dat<-read.csv("Data/VBG_4SeasonStdTemp.csv")
 
 str(dat)
 library(R2jags)
 
 # load data
 data <- list(Lr=dat$SVL.1, Lm = dat$SVL, dSp = dat$dSp, dSm = dat$dSm,
-             dW = dat$dW, dF= dat$dF, n = dim(dat)[1],
+             dW = dat$dW, dF= dat$dF, tempSp = dat$TempSp, tempSm = dat$TempSm,
+             tempF = dat$TempF, tempW = dat$TempW, n = dim(dat)[1],
              m=as.numeric(dat$Color), J=length(unique(dat$Color)))
 
 
 # Initial values
-inits <- function(){list(L.inf = rnorm(2,0,0.001))}
+inits <- function(){list(L.inf = rnorm(2,0,0.001), K = rnorm(4,1,0.001))}
 
 # Parameters monitored
-params1 <- c("L.inf", "Ksp", "Ksm","Kf","Kw", "sigma.Lr","tau.Lr")
+params1 <- c("L.inf", "Ksp", "Ksm","Kf","Kw", "sigma.Lr","tau.Lr",
+             "B0sp", "Btsp","B0sm", "Btsm","B0f", "Btf","B0w", "Btw")
 
 
 # MCMC settings
@@ -32,14 +34,14 @@ nc <- 3
 start.time = Sys.time()         # Set timer
 
 out <- jags(data = data, inits = inits, parameters.to.save = params1, 
-            model.file = "Models/Morph_4SeasonsStd.txt", n.chains = nc, n.thin = nt, n.iter = ni, 
+            model.file = "Models/Morph_4SeasonsStdTemp.txt", n.chains = nc, n.thin = nt, n.iter = ni, 
             n.burnin = nb)
 
 end.time = Sys.time()
 elapsed.time = round(difftime(end.time, start.time, units='mins'), dig = 2)
 cat('Posterior computed in ', elapsed.time, ' minutes\n\n', sep='') 
 
-save(out, file="Results/4SeasonStd.RData")
+save(out, file="Results/4SeasonStdTemp_All4.RData")
 # Calculate computation time
 
 
